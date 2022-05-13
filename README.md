5d51d06d923c29fb1a816b9465ea243d44a06145
Jess Schueler <jessicagrace.schueler@gmail.com>
2022-05-13 16:54:15 -0700

    Add merge method and Readme file.

---
+++ README.md
@@ -0,0 +1,31 @@
+# Spotify Data Exploration
+### by Jess Schueler
+
+#### A SQLAlchemy and Pandas CSV -> Mariadb data pipeline.
+
+## Technologies Used
+
+*Python
+*Pandas
+*SQLAlchemy
+
+# Description
+A basic pipeline that ingests CSV data and outputs it into a MariaDB database.
+
+##Setup/Installation Requirements
+
+*In the terminal, clone github repository using the following command;
+```
+$ git clone https://github.com/jessgschueler/spotifydb-engine
+```
+*In a venv, Pip install requirements.txt file
+*Start the database using the start.sh file
+*Run main.py
+
+##Known Bugs
+none
+
+##License
+MIT
+
+Copyright (c) 5/13/22 Jess Schueler
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+from numpy import inner, outer
 import pandas as pd
 import sqlalchemy as sa
 from sqlalchemy import create_engine, text
-from sqlalchemy import MetaData
-from sqlalchemy.sql import text
+from sqlalchemy import MetaData, Table, Column, Integer, String, Numeric, Text
+
 
 
 class DataLoader():
@@ -15,6 +16,7 @@ class DataLoader():
             filepath (str): file path to the CSV file
             index: default to none
         """
+        #reads csv and exports to dataframe
         df = pd.read_csv(filepath, header = 0)
         self.df = df
 
@@ -22,7 +24,8 @@ class DataLoader():
         """
         prints the head of the dataframe to console
         """
-        return self.df.head()
+        #prints first 5 rows to console
+        print(self.df.head())
 
     def add_index(self, index_name:str, column_names:list) -> None:
         """
@@ -35,9 +38,13 @@ class DataLoader():
             index_name (str): the index column name
             colum_names (list): list of columns to concatenate into an index column
         """
+        #defines df
         df = self.df
+        #applies lambda function on axis 1 (columns) to join specified with a "-", creates new column with the value
         df[index_name] = df[column_names].apply(lambda row: "-".join(row.values.astype(str)), axis=1)
+        #sets new column as index
         df.set_index(index_name, inplace= True)
+        #redefines df
         self.df = df
 
     def sort(self, column_name:str) -> None:
@@ -47,6 +54,7 @@ class DataLoader():
         Args:
             column_name (str): column name to sort by
         """
+        #sorts dataframe by a column, ascending. 
         return self.df.sort_values(column_name, axis = 0, ascending = True, inplace = False)
 
     def load_to_db(self, db_engine, db_table_name:str) -> None:
@@ -57,11 +65,22 @@ class DataLoader():
             db_engine (SqlAlchemy Engine): SqlAlchemy engine (or connection) to use to insert into database
             db_table_name (str): name of database table to insert to
         """
-        #
-        self.df.to_sql(name=db_table_name, con=self.engine, if_exists='append', chunksize=2000)
+        #load dataframes into table
+        self.df.to_sql(name=db_table_name, con=db_engine, if_exists='append', chunksize=2000, index=False)
 
+    def merge_df(self, right_df, left_on, right_on, how, inplace=True) -> None:
+        """
+        Merge dataframes
+        """
+        self.df= pd.merge(left=self.df, right=right_df, left_on=left_on, right_on=right_on, how=how)
 
-
+def merged_artist_album():
+    """
+    merges artist and album dataframes on id and artist id
+    """
+    artist_loader = DataLoader('./data/spotify_artists.csv', index = 'id')
+    album_loader = DataLoader('./data/spotify_albums.csv')
+    artist_loader.merge_df(album_loader.df,'id','artist_id','outer')
 
 
 def db_engine(db_host:str, db_user:str, db_pass:str, db_name:str="spotify") -> sa.engine.Engine:
@@ -96,12 +115,12 @@ def db_create_tables(db_engine, drop_first:bool = False) -> None:
 
     #define tables
     artists_table = Table("artists",
-        metadata,
+        meta,
         Column('index', Numeric),
-        Column('artist_poularity', Numeric),
+        Column('artist_popularity', Numeric),
         Column('followers', Numeric),
         Column('genres', String(10240)),
-        Column('id', Numeric, primary_key=True),
+        Column('id', String(256), primary_key=True),
         Column('name', String(256)),
         Column('track_id', String(256)),
         Column('track_name_prev', String(256)),
@@ -110,16 +129,17 @@ def db_create_tables(db_engine, drop_first:bool = False) -> None:
     )
 
     albums_table = Table("albums",
-        metadata,
+        meta,
         Column('index', Numeric),
         Column('album_type', String(256)),
         Column('artist_id', String(256)),
-        Column('available_markets', String(10240)),
+        Column('available_markets', Text),
         Column('external_urls', String(256)),
         Column('href', String(256)),
         Column('id', String(256), primary_key=True),
-        Column('name', String(10240)),
-        Column('release_date', DateTime, nullable=True),
+        Column('images', Text),
+        Column('name', String(256)),
+        Column('release_date', String(256)),
         Column('release_date_precision', String(256)),
         Column('total_tracks', Numeric),
         Column('track_id', String(256)),
@@ -131,34 +151,32 @@ def db_create_tables(db_engine, drop_first:bool = False) -> None:
 
     #drop tables is drop_first = True
     if drop_first:
-        metadata.drop_all()
+        meta.drop_all()
     
     #create tables
-    metadata.create_all(checkfirst=True)
+    meta.create_all(checkfirst=True)
 
 
 def main():
     """
     Pipeline Orchestratation method.
 
-    Performs the following:
-    - Creates a DataLoader instance for artists and albums
-    - prints the head for both instances
-    - Sets artists index to id column
-    - Sets albums index to artist_id, name, and release_date
-    - Sorts artists by name
-    - creates database engine
-    - creates database metadata tables/columns
-    - loads both artists and albums into database
     """
+    #Creates a DataLoader instance for artists and albums
     artist_loader = DataLoader('./data/spotify_artists.csv', index = 'id')
     album_loader = DataLoader('./data/spotify_albums.csv')
+    # prints the head for both instances
     artist_loader.head()
     album_loader.head()
+    #Sets albums index to artist_id, name, and release_date
     album_loader.add_index('index', ['artist_id','name','release_date'])
+    #Sorts artists by name
     artist_loader.sort('name')
+    #creates database engine
     engine = db_engine('127.0.0.1:3306', 'root', 'mysql')
+    #creates database metadata tables/columns
     db_create_tables(engine, drop_first = True)
+    #loads both artists and albums into database
     artist_loader.load_to_db(engine, 'artists')
     album_loader.load_to_db(engine, 'albums')
     
